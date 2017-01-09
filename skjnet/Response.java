51f348360ac162383f1c8ccad988e38fad77c933
Maciej Caputa <maciek.caputa@gmail.com>
2017-01-09 20:50:38 +0100

    Response class cleanup

--- skjnet/Response.java
+++ skjnet/Response.java
@@ -1,11 +1,8 @@
 package skjnet;
 
-import java.io.BufferedReader;
+
 import java.io.DataInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -21,37 +18,22 @@ public class Response {
 	}
 	
 	public void readHeaders() {
-		
+		DataInputStream inFromServer = new DataInputStream(is);
+		String line;
 		try {
-//			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(is));
-			DataInputStream inFromServer = new DataInputStream(is);
-			
-			
-			String line;
 			status = inFromServer.readLine();
-			//if not SKJNET protocol close connection:
-			 if (!ok()) {
-				valid = false;
-				return;
-			 }
-			
+			if (!ok()) throw new Exception();
 		    while((line=inFromServer.readLine())!=null){ 
 				 if (line.isEmpty()) break;
 				 String[] h = line.split(":");
-
 				 headers.put(h[0], h[1]);
 			 }
-		    
-//		    inFromServer.close();
-		    
 		    valid = true;
-		    
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+		} catch (Exception e) {
+			valid = false;
+			System.out.println("Cannot read header");
+			return;
+		}	
 	}
 	
 	public boolean valid() {
